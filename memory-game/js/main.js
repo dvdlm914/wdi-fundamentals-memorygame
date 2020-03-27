f5d911777898b9ae7d3ca5a21e4297092e02b7e8
DAVID MYRICK <dvdlm914@DAVIDs-MacBook-Pro.local>
2020-03-27 12:05:02 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardThree);
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User Flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match");
+	}
+	else { 
+		alert("Sorry try again");
+	}
+}
+
